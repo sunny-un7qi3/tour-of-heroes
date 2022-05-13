2d21c7d53c6b5e52ee3560d7322696cae6e447cb
Sunny <sunny@un7qi3.co>
2022-05-13 12:16:47 +0900

    검색창 구현

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import {HttpClientModule} from "@angular/common/http";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 
@@ -24,7 +25,8 @@ import {InMemoryDataService} from "./in-memory-data.service";
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
